7944d5f4b581a80dfc72986b4e701081a58a0dd6
Guido <guidolorenzotti@hotmai.com>
2022-06-15 00:40:07 -0300

    funcionalidad cancelar inscripcion e imprimir listado de alumnos inscriptos

--- misinscripciones.php
+++ misinscripciones.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
 <?php
 include("head.html");
@@ -9,15 +8,30 @@ include("head.html");
 <body> 
 
 <?php
-include("headerAlumno.html");
 include("conexion.inc");
 $vIDalumno = 1; #$_POST ['id_alumno'];
+
+if (!empty($_POST ['actionType']) && !empty($_POST["inputIDconsulta"]) && $_POST ['actionType']=="cancelar") {
+    $vIDconsulta = $_POST["inputIDconsulta"];
+    $vSql = "UPDATE inscripciones SET estado_inscripcion = 4 WHERE id_consulta = '$vIDconsulta' and id_alumno = '$vIDalumno'";
+    if(mysqli_query($link, $vSql)) {
+        $vTipoMensaje = "success";
+        $vMensaje = "Se ha cancelado la inscripción";
+    }
+    else{
+        $vTipoMensaje = "danger";
+        $vMensaje = "Ha ocurrido un error, intente nuevamente";
+    }
+}
+
+include("headerAlumno.html");
+include("mensaje.php");
 
 $vSql = "SELECT * FROM inscripciones i inner join alumnos a on i.id_alumno = a.legajo
                                      inner join consultas c on i.id_consulta = c.id_consulta
                                      inner join profesores p on p.id_profesor = c.id_profesor
                                      inner join materias m on m.id_materia = c.id_materia
-                                    where i.id_alumno = '$vIDalumno'";
+                                    where i.id_alumno = '$vIDalumno' and i.estado_inscripcion != 4";
 $vResultado = mysqli_query($link, $vSql);
 
 ?>
@@ -42,7 +56,12 @@ while ($fila = mysqli_fetch_array($vResultado))
             <td><?php echo ($fila['hora_consulta']); ?></td>
             <td><?php echo ($fila['nombre_materia']); ?></td>
             <td><?php echo ($fila['nombre_apellido']); ?></td>
-            <td style="text-align:center" ;><button type="button" class="btn btn-danger"> Cancelar inscripcion </button></td>
+            <td> 
+                <form action="misinscripciones.php" method="post">
+                    <input name="inputIDconsulta" type="text" class="form-control" style="display:none" id="inputIDconsulta" value="<?php echo ($fila['id_consulta']); ?>">
+                    <button type="submit" name="actionType" value="cancelar" class="btn btn-danger"> Cancelar inscripcion </button>
+                </form>
+            </td>
         </tr>
         <?php
 }
